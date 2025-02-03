9c4611a0418082198c420dba87788da1535ad3ea
flickeringsight <flickeringsight@gmail.com>
2025-02-03 11:04:27 -0800

    Add files via upload

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1853,7 +1853,7 @@
 //#define Z_AFTER_HOMING         10   // (mm) Height to move to after homing (if Z was homed)
 //#define XY_AFTER_HOMING { 10, 10 }  // (mm) Move to an XY position after homing (and raising Z)
 
-#define EVENT_GCODE_AFTER_HOMING "G0 X0 Y0"  // Commands to run after G28 (and move to XY_AFTER_HOMING) //abj
+#define EVENT_GCODE_AFTER_HOMING "G0 X0 Y0"  // Commands to run after G28 (and move to XY_AFTER_HOMING) //abj -- XY_AFTER_HOMING doesn't work for some raisin.
 
 // Direction of endstops when homing; 1=MAX, -1=MIN
 // :[-1,1]
