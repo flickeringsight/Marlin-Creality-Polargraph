fcc4fbda0f23c7b2a86027e4ad6426e54ba335b1
flickeringsight <flickeringsight@gmail.com>
2025-02-06 09:24:33 -0800

    Adjusted Pen Up and Pen Down servo angles.

--- Marlin/src/lcd/menu/menu_motion.cpp
+++ Marlin/src/lcd/menu/menu_motion.cpp
@@ -549,8 +549,8 @@ void menu_motion() {
   // Pen up/down menu
   //
   #if ENABLED(PEN_UP_DOWN_MENU)
-    GCODES_ITEM(MSG_MANUAL_PENUP, F("M280 P0 S90"));
-    GCODES_ITEM(MSG_MANUAL_PENDOWN, F("M280 P0 S50"));
+    GCODES_ITEM(MSG_MANUAL_PENUP, F("M280 P0 S50"));
+    GCODES_ITEM(MSG_MANUAL_PENDOWN, F("M280 P0 S90"));
   #endif
 
   //
