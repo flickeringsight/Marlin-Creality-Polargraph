0a3677b33cfc92e3ebe95f3572762c31eb8bc9e8
flickeringsight <flickeringsight@gmail.com>
2025-02-06 12:56:34 -0800

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -3683,7 +3683,7 @@
 // (ms) Delay before the next move will start, to give the servo time to reach its target angle.
 // 300ms is a good value but you can try less delay.
 // If the servo can't reach the requested position, increase it.
-#define SERVO_DELAY { 1500 } //abj
+#define SERVO_DELAY { 500 } //abj
 
 // Only power servos during movement, otherwise leave off to prevent jitter
 #define DEACTIVATE_SERVOS_AFTER_MOVE //abj
